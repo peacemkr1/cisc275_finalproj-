753fc3b1a3d535344e639282647fe3db2ca1e820
David Cardenas <davidcardenas14@yahoo.com>
2025-03-18 21:25:41 -0400

    Added my name

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. 
+        <div>David Cardenas</div>
         </p>
         <a
           className="App-link"

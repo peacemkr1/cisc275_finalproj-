f53f618f7e2ff5f54ba57fda1f8c75d305230e4f
Ayman Tayeb <atayeb@udel.edu>
2025-03-27 17:32:06 -0400

    change to name

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <div>Ayman Tayeb</div>
         </p>
-        <div>Ayman Tayeb</div>
+        
         <a
           className="App-link"
           href="https://reactjs.org"

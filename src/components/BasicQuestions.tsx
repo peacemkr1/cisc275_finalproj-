c3f3241cba0014487532546c77027143d94fc2b5
David Cardenas <davidcardenas14@yahoo.com>
2025-05-10 23:55:20 -0400

    First I must report that the css styling changes i mentioned in my last commit message where i move styling in ProgressBar.tsx to App.css is reflected here i forgot to add that file in my last commit I am very please to report that the styling for basic questions is up to our expectations and is much more appealing than what we had before. I have made all the colors of the buttons more uniform and have addressed collisons with some of the buttons that would pop up at the end of the quiz once where 'Get Results' and 'View Career Results' would just push away 'next' and 'previous'  buttons into odd locations

--- src/components/BasicQuestions.tsx
+++ src/components/BasicQuestions.tsx
@@ -6,7 +6,17 @@ import { generateBasicCareer } from './ChatGPT';
 import loadingGif from '../loadingScreen.gif';
 import '../App.css';
 
-
+const QUESTIONS = [
+  { prompt: 'What was your favorite subject in school?', options: ['Math','Science','English','History'] },
+  { prompt: 'How would you describe your level of talkativeness?', options: ['Very Talkative','Somewhat Talkative','Quiet','Depends on Situation'] },
+  { prompt: 'How comfortable are you with public speaking?', options: ['Very Comfortable','Somewhat Comfortable','Uncomfortable','Prefer To Avoid It'] },
+  { prompt: 'What is your preferred way of communicating?', options: ['In Person','Phone Call','Text','Email'] },
+  { prompt: 'What equipment do you like to work with?', options: ['Computers','Tools','Instruments','Art Supplies'] },
+  { prompt: 'How would you describe yourself when dealing with stress or pressure?', options: ['Stay Calm','Get Focused','Feel Overwhelmed','Avoid the Situation'] },
+  { prompt: 'When working in a group, what role do you take?', options: ['Leader','Organizer','Contributor','Supporter'] },
+  { prompt: 'Which work environment do you prefer?', options: ['In an office with others','Hybrid (home and office)','Fully remote','Hands-on/outdoors'] },
+  { prompt: 'Which career field are you most drawn to?', options: ['Healthcare','Business','Law','Engineering','Theatre'] }
+];
 
 interface BasicQuestionsProps {
   onProgressUpdate: (progress: number) => void;
@@ -26,21 +36,13 @@ export function BasicQuestions({ onProgressUpdate }: BasicQuestionsProps): JSX.E
       I referenced the TOME to complete this section: 
         https://frontend-fun.github.io/react-hooks-typescript-tome/4-state/forms.html#multiline-textarea
   */
-  const [Question1, setQuestion1] = useState<string>('');
-  const [Question2, setQuestion2] = useState<string>('');
-  const [Question3, setQuestion3] = useState<string>('');
-  const [Question4, setQuestion4] = useState<string>('');
-  const [Question5, setQuestion5] = useState<string>('');
-  const [Question6, setQuestion6] = useState<string>('');
-  const [Question7, setQuestion7] = useState<string>('');
-  const [Question8, setQuestion8] = useState<string>('');
-  const [Question9, setQuestion9] = useState<string>('');
+  const [answers, setAnswers] = useState<string[]>(Array(QUESTIONS.length).fill(''));
+  const showButton = answers.every(a => a !== '');
 
   // New state for current question index
   const [currentQuestion, setCurrentQuestion] = useState<number>(0);
 
 
-  const [showButton, setShowButton] = useState<boolean>(false); // set to false
   //const [showFeedbackMech, setFeedbackMech] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [showResults, setShowResults] = useState<boolean>(false);
@@ -52,28 +54,13 @@ export function BasicQuestions({ onProgressUpdate }: BasicQuestionsProps): JSX.E
 
 
   useEffect(() => {
-    const totalQuestions = 9;
-    const answeredQuestions = [
-      Question1, Question2, Question3, Question4, Question5,
-      Question6, Question7, Question8, Question9
-    ].filter(answer => answer !== '').length;
-    const progressPercentage = (answeredQuestions / totalQuestions) * 100;
-    onProgressUpdate(progressPercentage);
-
-    if (progressPercentage === 100) {
-      setShowButton(true);
-      if (!hasShownPopup) {
-        setShowPopup(true);
-        setHasShownPopup(true);
-      }
-    } else {
-      setShowButton(false);
+    const answeredCount = answers.filter(a => a !== '').length;
+    onProgressUpdate((answeredCount / QUESTIONS.length) * 100);
+    if (answeredCount === QUESTIONS.length && !hasShownPopup) {
+      setShowPopup(true);
+      setHasShownPopup(true);
     }
-  }, [
-    Question1, Question2, Question3, Question4, Question5,
-    Question6, Question7, Question8, Question9,
-    hasShownPopup, onProgressUpdate
-  ]);
+  }, [answers, hasShownPopup, onProgressUpdate]);
   
   
 
@@ -81,7 +68,6 @@ export function BasicQuestions({ onProgressUpdate }: BasicQuestionsProps): JSX.E
 
   async function handleGetAnswer() {
     setLoading(true);
-    const answers = [Question1, Question2, Question3, Question4, Question5, Question6, Question7, Question8, Question9];
     const result = await generateBasicCareer(answers);
     setCareerResult(result || 'No result found. Please try again.');
     setLoading(false);
@@ -108,201 +94,32 @@ export function BasicQuestions({ onProgressUpdate }: BasicQuestionsProps): JSX.E
           */}
 
           <Form className="basic-form" onSubmit={(e) => e.preventDefault()}>
-            {/* Question 1 */}
-            {currentQuestion === 0 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  What was your favorite subject in school?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['Math','Science','English','History'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question1 === option}
-                      onClick={() => setQuestion1(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 2 */}
-            {currentQuestion === 1 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  How would you describe your level of talkativeness?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['Very Talkative','Somewhat Talkative','Quiet','Depends on Situation'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question2 === option}
-                      onClick={() => setQuestion2(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 3 */}
-            {currentQuestion === 2 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  How comfortable are you with public speaking?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['Very Comfortable','Somewhat Comfortable','Uncomfortable','Prefer To Avoid It'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question3 === option}
-                      onClick={() => setQuestion3(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 4 */}
-            {currentQuestion === 3 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  What is your preferred way of communicating?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['In Person','Phone Call','Text','Email'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question4 === option}
-                      onClick={() => setQuestion4(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 5 */}
-            {currentQuestion === 4 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  What equipment do you like to work with?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['Computers','Tools','Instruments','Art Supplies'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question5 === option}
-                      onClick={() => setQuestion5(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 6 */}
-            {currentQuestion === 5 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  How would you describe yourself when dealing with stress or pressure?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['Stay Calm','Get Focused','Feel Overwhelmed','Avoid the Situation'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question6 === option}
-                      onClick={() => setQuestion6(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 7 */}
-            {currentQuestion === 6 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  When working in a group, what role do you take?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['Leader','Organizer','Contributor','Supporter'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question7 === option}
-                      onClick={() => setQuestion7(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 8 */}
-            {currentQuestion === 7 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  Which work environment do you prefer?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['In an office with others','Hybrid (home and office)','Fully remote','Hands-on/outdoors'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question8 === option}
-                      onClick={() => setQuestion8(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
-
-            {/* Question 9 */}
-            {currentQuestion === 8 && (
-              <Form.Group className="form-group-custom">
-                <Form.Label style={{ fontWeight: 'bold' }}>
-                  Which career field are you most drawn to?
-                </Form.Label>
-                <ListGroup className="listgroup-fullwidth">
-                  {['Healthcare','Business','Law','Engineering','Theatre'].map(option => (
-                    <ListGroup.Item
-                      key={option}
-                      action
-                      active={Question9 === option}
-                      onClick={() => setQuestion9(option)}
-                    >
-                      {option}
-                    </ListGroup.Item>
-                  ))}
-                </ListGroup>
-              </Form.Group>
-            )}
+            <Form.Group className="form-group-custom">
+              <Form.Label>{QUESTIONS[currentQuestion].prompt}</Form.Label>
+              <ListGroup className="listgroup-fullwidth">
+                {QUESTIONS[currentQuestion].options.map(option => (
+                  <ListGroup.Item
+                    key={option}
+                    action
+                    active={answers[currentQuestion] === option}
+                    onClick={() => {
+                      const newAnswers = [...answers];
+                      newAnswers[currentQuestion] = option;
+                      setAnswers(newAnswers);
+                    }}
+                  >
+                    {option}
+                  </ListGroup.Item>
+                ))}
+              </ListGroup>
+            </Form.Group>
           </Form>
           
         </div>
       </div>
 
       {/* Navigation Buttons */}
-      <div className="nav-buttons">
+      <div className={`nav-buttons ${currentQuestion === 8 ? 'single-nav' : ''}`}>
         <Button
           type="button"
           variant="secondary"
@@ -311,32 +128,38 @@ export function BasicQuestions({ onProgressUpdate }: BasicQuestionsProps): JSX.E
         >
           Previous
         </Button>
-        {currentQuestion < 8 && (
+
+        {showButton && (
+          <Button
+            type="button"
+            variant="secondary"
+            onClick={handleGetAnswer}
+          >
+            Get Answer
+          </Button>
+        )}
+
+        {currentQuestion < QUESTIONS.length - 1 && (
           <Button
             type="button"
             variant="secondary"
             onClick={() => setCurrentQuestion(prev => prev + 1)}
-            disabled={
-              [
-                Question1, Question2, Question3, Question4,
-                Question5, Question6, Question7, Question8, Question9
-              ][currentQuestion] === ''
-            }
+            disabled={!answers[currentQuestion]}
           >
             Next
           </Button>
         )}
       </div>
 
-      {(showButton === true && currentQuestion === 8) && (
+      {showButton && (
         <> 
         {/*https://react-bootstrap.netlify.app/docs/components/modal/ */}
-          <div style={{ textAlign: "center", marginTop: "2rem" }}>
+          <div className="completion-popup">
             <Modal show={showPopup} onHide={() => setShowPopup(false)} centered>
               <Modal.Header closeButton>
                 <Modal.Title>All Questions Answered!</Modal.Title>
               </Modal.Header>
-              <Modal.Body style={{ fontWeight: 'bold' }}>
+              <Modal.Body>
                 Great job! You've completed all the quiz questions. If you'd like to review or revise your answers, feel free to scroll up and make changes. When you're ready, click <strong>'Get Answer'</strong> to view your personalized career suggestions!
               </Modal.Body>
               <Modal.Footer>
@@ -346,26 +169,16 @@ export function BasicQuestions({ onProgressUpdate }: BasicQuestionsProps): JSX.E
               </Modal.Footer>
             </Modal>
     
-            <div style={{ display: "flex", justifyContent: "center", gap: "1rem", marginTop: "2rem" }}>
-              <Button 
-                type="button"
-                style={{ backgroundColor: "green", border: "none" }} 
-                onClick={handleGetAnswer}
+            {careerResult && (
+              <Button
+                className="toggle-results-btn"
+                onClick={() => setShowResults(prev => !prev)}
               >
-                Get Answer
+                {showResults ? 'Hide Career Results' : 'View Career Results'}
               </Button>
-    
-              {careerResult && (
-                <Button 
-                  onClick={() => setShowResults(prev => !prev)}
-                  style={{ backgroundColor: 'LightSalmon', border: 'none' }}
-                >
-                  {showResults ? 'Hide Career Results' : 'View Career Results'}
-                </Button>
-              )}
-            </div>
+            )}
             {showResults && careerResult && (
-              <div style={{ marginTop: '2rem', backgroundColor: 'white', padding: '1rem', borderRadius: '8px', textAlign: 'center' }}>
+              <div className="results-card">
                 <h4>Career Suggestions:</h4>
                 <div style={{ whiteSpace: 'pre-wrap' }}>{careerResult}</div>
               </div>
@@ -373,9 +186,9 @@ export function BasicQuestions({ onProgressUpdate }: BasicQuestionsProps): JSX.E
           </div>
     
           <Modal show={loading} centered backdrop="static" keyboard={false}>
-            <Modal.Body style={{ textAlign: 'center', padding: '2rem' }}>
-              <img src={loadingGif} alt="Loading..." style={{ width: '80px', marginBottom: '1rem' }} />
-              <div style={{ fontWeight: 'bold', fontSize: '18px' }}>Generating results...</div>
+            <Modal.Body className="loading-modal-body">
+              <img src={loadingGif} alt="Loading..." className="loading-spinner" />
+              <div className="loading-text">Generating results...</div>
             </Modal.Body>
           </Modal>
         </>

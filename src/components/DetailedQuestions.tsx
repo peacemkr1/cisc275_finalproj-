b644c3015eb9e1ab1a0225652d308aac5717e792
Ayman Tayeb <atayeb@udel.edu>
2025-05-11 23:32:41 -0400

    changed experience to job growth rate for a better report response because the experience section was confusing. also this was a bug in our code because when we tried to generate results nothing would appear until now

--- src/components/DetailedQuestions.tsx
+++ src/components/DetailedQuestions.tsx
@@ -178,7 +178,7 @@ const DetailedQuestions = ({ onProgressUpdate }: DetailedQuestionsProps): JSX.El
                   <tr>
                     <th>Career Name</th>
                     <th>Education</th>
-                    <th>Experience</th>
+                    <th>Job Growth Rate</th>
                     <th>Salary</th>
                     <th>Match</th>
                   </tr>
@@ -188,7 +188,7 @@ const DetailedQuestions = ({ onProgressUpdate }: DetailedQuestionsProps): JSX.El
                     <tr key={idx}>
                       <td>{job.career}</td>
                       <td>{job.education}</td>
-                      <td>{job.experience}</td>
+                      <td>{job.jobGrowth}</td>
                       <td>{job.salary}</td>
                       <td>{job.match}</td>
                     </tr>
